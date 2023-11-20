09c7568275e2c3f576613ac3e282184ec9db4e74
Виктор Лобынцев <sibfight@mail.ru>
2023-11-21 00:37:51 +0300

    1day

---
+++ src/reportWebVitals.js
@@ -0,0 +1,14 @@
+// reportWebVitals.js
+// import { getCLS, getFID, getFCP, getLCP, getTTFB } from "web-vitals";
+
+// const sendToAnalytics = ({ name, delta, id }) => {
+//   // Отправить данные аналитики
+// };
+
+// getCLS(sendToAnalytics);
+// getFID(sendToAnalytics);
+// getFCP(sendToAnalytics);
+// getLCP(sendToAnalytics);
+// getTTFB(sendToAnalytics);
+
+// export default sendToAnalytics;
